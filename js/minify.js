1d59c2119ee8cafaa606fae53ad96a232f0b2e01
Wenfeng Qiu <wenfengqiu@hotmail.com>
2021-03-20 22:49:32 -0700

    Remove test js

--- js/minify.js
+++
@@ -1,9 +0,0 @@
-var compressor = require('node-minify');
- 
-// Using Google Closure Compiler
-compressor.minify({
-  compressor: 'gcc',
-  input: './controller_1.0.1.js',
-  output: './controller_1.0.1.min.js',
-  callback: function(err, min) {}
-});
